cfab885b0d2217c606b0912ce467597c46fd5127
mathysv-irvis <mathys.v@i-rvis.com>
2026-06-16 11:31:45 +0900

    fix/ files fix with new asynchronous detection

--- detection/detection.py
+++ detection/detection.py
@@ -2,22 +2,22 @@ from .filter import process_kmeans, process_lab, process_contrast, process_bw
 from .filter import process_morphology, process_largest_component
 import cv2
 
-def filter_kmeans_augmented(image, cluster):
+def filter_kmeans_augmented(image, cluster, K, max_component, threshold=130, kernel_size=7):
 
     _, morph_mask = process_morphology(
         image,
-        threshold=130,
-        kernel_size=7,
+        threshold=threshold,
+        kernel_size=kernel_size,
     )
 
     kmean, mask = process_kmeans(
         image=image,
         roi_mask=morph_mask,
         cluster=cluster,
-        K=4,
+        K=K,
     )
 
-    disp_postproc, mask_postproc = process_largest_component(mask, threshold=1400)
+    disp_postproc, mask_postproc = process_largest_component(mask, threshold=max_component)
 
     display = cv2.bitwise_and(
         image,
