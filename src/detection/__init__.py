4a9740b56a5609087c9df8cc578f0cc13673f316
mathysv-irvis <mathys.v@i-rvis.com>
2026-06-22 10:55:19 +0900

    fix/ import of kmeans augmented filter

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,6 +1,6 @@
 from .camera import CameraSim, CameraRunner
 from .filter import process_contrast, process_color, process_gradient, process_background, process_contour, process_lab, process_kmeans, process_bw, process_morphology, process_largest_component
-
+from .detection import filter_kmeans_augmented
 __all__= [
     "CameraRunner",
     "CameraSim",
@@ -15,4 +15,6 @@ __all__= [
     "process_bw",
     "process_morphology",
     "process_largest_component",
+
+    "filter_kmeans_augmented",
 ]
